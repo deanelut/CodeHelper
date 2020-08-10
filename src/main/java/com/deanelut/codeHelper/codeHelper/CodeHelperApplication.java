91cdf812ac7f3a877e5eb859b4488c350bf2340f
deanelut <deanelut@gmail.com>
2020-08-10 11:32:38 -0700

    Add files via upload

---
+++ src/main/java/com/deanelut/codeHelper/codeHelper/CodeHelperApplication.java
@@ -0,0 +1,13 @@
+package com.deanelut.codeHelper.codeHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deanelut/codeHelper/codeHelper/CodeHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deanelut.codeHelper.codeHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
